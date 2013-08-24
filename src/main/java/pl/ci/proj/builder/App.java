4c0849dcbf26ba1e7a8e574b485375d308a105a9
unknown <przemyslaws@dickgrayson.kainos.com>
2013-08-24 17:40:07 +0200

    Initial revision

---
+++ src/main/java/pl/ci/proj/builder/App.java
@@ -0,0 +1,13 @@
+package pl.ci.proj.builder;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
